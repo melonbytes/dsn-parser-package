92df6f1d30ff8375bfd2f89afbc690ca80b1f649
Ray Bramwell <ray@Rays-MBP-16.localdomain>
2021-07-06 21:14:32 +0100

    Initial Commit

---
+++ src/DSNParser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DSNParser;
+
+class DSN {
+    public $connection_string;
+    const DSN_FORMAT = '/^([[:alnum:]]+):\/\/([[:alnum:]_-]+):([[:ascii:]]+)@([[:alnum:].-]+)\/([[:alnum:]_-]+)$/';
+
+    function parse($connection_string) {
+        $dsn = new stdClass;
+        $dsn->dsn = $connection_string;
+        if (preg_match(self::DSN_FORMAT,$connection_string,$matches)) {
+            $dsn->protocol = $matches[1];
+            $dsn->username = $matches[2];
+            $dsn->password = $matches[3];
+            $dsn->hostname = $matches[4];
+            $dsn->database = $matches[5];
+        } else {
+            $dsn->protocol = null;
+            $dsn->username = null;
+            $dsn->password = null;
+            $dsn->hostname = null;
+            $dsn->database = null;
+        }
+        return $dsn;
+    }
+}
+
+$secret1 = "mariadb://test_usr:Nfuih@e9v:4/fsdv_dsf.dfsS3@local-proxysql.localdomain/test_db";
+$secret2 = "invalid_string";
+$conn = new DSN();
+print_r($conn->parse($secret1));
+print_r($conn->parse($secret2));
