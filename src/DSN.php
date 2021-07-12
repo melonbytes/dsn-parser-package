694c6dbcd777477ec136ba12cf6bef7d89de7de5
Ray Bramwell <dev@melonbytes.com>
2021-07-12 19:23:23 +0100

    Added support for declaring port number in DSN

--- src/DSN.php
+++ src/DSN.php
@@ -4,7 +4,7 @@ namespace MelonBytes\DSNParser;
 
 class DSN {
     public $connection_string;
-    const DSN_FORMAT = '/^([[:alnum:]]+):\/\/([[:alnum:]_-]+):([[:ascii:]]+)@([[:alnum:].-]+)\/([[:alnum:]_-]+)$/';
+    const DSN_FORMAT = '/^([[:alnum:]]+):\/\/([[:alnum:]_-]+):([[:ascii:]]+)@([[:alnum:].-]+):*([0-9]*)\/([[:alnum:]_-]+)$/';
 
     function parse($connection_string) {
         $dsn = new \stdClass;
@@ -14,13 +14,33 @@ class DSN {
             $dsn->username = $matches[2];
             $dsn->password = $matches[3];
             $dsn->hostname = $matches[4];
-            $dsn->database = $matches[5];
+            $dsn->port     = $matches[5];
+            $dsn->database = $matches[6];
         } else {
             $dsn->protocol = null;
             $dsn->username = null;
             $dsn->password = null;
             $dsn->hostname = null;
+            $dsn->port     = null;
             $dsn->database = null;
+        }
+        if (!is_numeric($dsn->port)) {
+            switch ($dsn-protocol) {
+                case "mariadb":
+                    $dsn->port = 3306;
+                    break;
+                case "mysql";
+                    $dsn->port = 3306;
+                    break;
+                case "sqlserver";
+                    $dsn->port = 1433;
+                    break;
+                case "pgsql";
+                    $dsn->port = 5432;
+                    break;
+                default:
+                    $dsn->port = 3306;
+            }
         }
         return $dsn;
     }
